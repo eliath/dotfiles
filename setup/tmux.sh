ab5d0ca58c2b4b184082251e8852fd25fbcb2492
Elias Martinez Cohen <ecohen@twitter.com>
2017-03-08 18:04:36 -0500

    Better way of managing tmux conf

--- setup/tmux.sh
+++ setup/tmux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 pushd "$(dirname "$0")" >> /dev/null
 
-ln -s "$DOTFILES/tmux/.tmux.conf" "$HOME/.tmux.conf"
+touch $HOME/.tmux.conf
+echo "source-file $DOTFILES/tmux/config" >> $HOME/.tmux.conf
 
 popd >> /dev/null
