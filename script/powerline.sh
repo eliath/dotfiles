4aafd5b6cf3819a94119ac9f87b109021e6163fe
Elias Martinez Cohen <eliasmartinezcohen@gmail.com>
2017-05-27 21:23:35 -0400

    Add configs for powerline and tmux plugins

---
+++ script/powerline.sh
@@ -0,0 +1,4 @@
+. common.sh
+pip install --user powerline-status
+mkdir "$HOME/.config"
+ln -s "$DOTFILES/powerline" "$HOME/.config/powerline"
--- script/tmux.sh
+++ script/tmux.sh
@@ -1,5 +1,7 @@
 . common.sh
 
+mkdir -p "$HOME/.tmux/plugins"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 rm -f $HOME/.tmux.conf
 ln -s $DOTFILES/tmux/.tmux.conf $HOME/.tmux.conf
 tmux source-file ~/.tmux.conf
