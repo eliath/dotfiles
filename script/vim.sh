88b7f2f5bafe396ad26671b15de8903c6f32a2f6
Elias Martinez Cohen <eliasmartinezcohen@gmail.com>
2017-05-27 22:20:18 -0400

    Fix vim linking bug

--- script/vim.sh
+++ script/vim.sh
@@ -1,6 +1,8 @@
 . common.sh
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+rm  -rf ~/.vim
+VIM_CONFIG_SRC="$DOTFILES/vim/.vim"
+curl -fLo $VIM_CONFIG_SRC/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -s "$DOTFILES/vim/.vim" "$HOME/.vim"
+ln -s $VIM_CONFIG_SRC "$HOME/.vim"
 ln -s "$DOTFILES/vim/.vimrc" "$HOME/.vimrc"
