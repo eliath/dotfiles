53eee6a419458bb12e1e06cc0251b84cc759dc89
Elias Martinez Cohen <eliasmartinezcohen@gmail.com>
2017-05-29 14:37:36 -0400

    Fix #1

--- script/brew.sh
+++ script/brew.sh
@@ -1,14 +1,20 @@
 . common.sh
 
+# Homebrew Script for OSX
+echo "Installing brew..."
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 brew update
 brew upgrade
 
 # dev tools
+brew install vim --with-override-system-vi
 brew install tmux
 brew install autojump
 brew install htop
 
-# cask apps
+# cask
+brew tap caskroom/cask
 brew cask install google-chrome
 brew cask install alfred
 brew cask install spectacle
