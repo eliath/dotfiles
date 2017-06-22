e35a2d073d64cd12829453c352f82be18f02b9a6
Elias Martinez Cohen <eliasmartinezcohen@gmail.com>
2017-06-22 12:11:24 -0400

    auto-install plugins for tmux

--- script/tmux.sh
+++ script/tmux.sh
@@ -4,4 +4,5 @@ mkdir -p "$HOME/.tmux/plugins"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 rm -f $HOME/.tmux.conf
 ln -s $DOTFILES/tmux/.tmux.conf $HOME/.tmux.conf
+~/.tmux/plugins/tpm/bin/install_plugins
 tmux source-file ~/.tmux.conf
