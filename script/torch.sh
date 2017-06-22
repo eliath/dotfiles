cc5cb9fab6cd0c9a326c0b6fa259c9a92a0541aa
Elias Martinez Cohen <eliasmartinezcohen@gmail.com>
2017-06-22 12:23:45 -0400

    Fix torch install

--- script/torch.sh
+++ script/torch.sh
@@ -1,18 +1,24 @@
-. lazt_init.sh
-
-# Install torch & some extra libs
-git clone https://github.com/torch/distro.git ~/torch --recursive
-pushd ~/torch
-bash install-deps
-./install.sh
-popd
+. lazy_init.sh
+TORCH_DIR='$HOME/torch'
+# Prevent multiple installs
+if [ -d "$TORCH_DIR/install" ]; then
+  echo '[DOTFILES] Torch seems to have already been installed. Skipping...'
+else
+  # Install torch & some extra libs
+  git clone https://github.com/torch/distro.git ~/torch --recursive
+  pushd ~/torch
+  bash install-deps
+  ./install.sh
+  popd
+fi
 
 # luarocks deps
-luarocks install async
-luarocks install luasocket
-luarocks install image
-luarocks install csvigo
-luarocks install graphicsmagick
-luarocks install luacheck
+LUAROCKS_BIN=$TORCH_DIR/install/bin/luarocks
+$LUAROCKS_BIN install async
+$LUAROCKS_BIN install luasocket
+$LUAROCKS_BIN install image
+$LUAROCKS_BIN install csvigo
+$LUAROCKS_BIN install graphicsmagick
+$LUAROCKS_BIN install luacheck
 
 ln -s $DOTFILES/lua/.luacheckrc $HOME/
