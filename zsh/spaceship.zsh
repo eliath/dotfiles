e07732ab977362325578979b7daa1e9373b7ad5a
Elias Martinez Cohen <emc@eliath.biz>
2020-12-05 13:40:05 -0500

    Adjust prompt to show battery and jobs indicator

--- zsh/spaceship.zsh
+++ zsh/spaceship.zsh
@@ -12,15 +12,20 @@ SPACESHIP_PROMPT_ORDER=(
   #kubectl       # Kubectl context section
   line_sep      # Line break
   vi_mode       # Vi-mode indicator
+  jobs          # Background jobs indicator
   char          # Prompt character
 )
 
 
 SPACESHIP_RPROMPT_ORDER=(
-  jobs          # Background jobs indicator
   exit_code     # Exit code section
   exec_time     # Execution time
+  battery       # power level
 )
+
+SPACESHIP_BATTERY_THRESHOLD=20
+SPACESHIP_EXIT_CODE_SHOW=true
+SPACESHIP_EXIT_CODE_COLOR=cyan
 
 autoload -U promptinit; promptinit
 prompt spaceship
