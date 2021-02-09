fc7ccb9455b07a7137b78d0fe1c5c695fe75e210
Elias Martinez Cohen <emc@eliath.biz>
2021-02-09 15:18:32 -0500

    [prompt] Fix rprompt on top line, Reorg

--- zsh/spaceship.zsh
+++ zsh/spaceship.zsh
@@ -4,24 +4,36 @@ SPACESHIP_PROMPT_ORDER=(
   dir           # Current directory section
   host          # Hostname section
   git           # Git section (git_branch + git_status)
-  node          # Node.js section
-  golang        # Go section
-  docker        # Docker section
-  aws           # Amazon Web Services section
-  pyenv         # Pyenv section
-  #kubectl       # Kubectl context section
   line_sep      # Line break
   vi_mode       # Vi-mode indicator
   jobs          # Background jobs indicator
+  exit_code     # Exit code section
   char          # Prompt character
 )
 
-
 SPACESHIP_RPROMPT_ORDER=(
-  exit_code     # Exit code section
+  rprompt_prefix
+  golang        # Go section
+  node          # Node.js section
+  aws           # Amazon Web Services section
+  docker        # Docker section
+  pyenv         # Pyenv section
   exec_time     # Execution time
   battery       # power level
+  #kubectl       # Kubectl context section
+  rprompt_suffix
 )
+
+# stolen from https://github.com/denysdovhan/spaceship-prompt/issues/558#issuecomment-451918947
+# > borrowed from __p9k_prepare_prompts
+# > RPROMPT_SUFFIX='%{'$'\e[1B''%}' # one line down
+# > RPROMPT_PREFIX='%{'$'\e[1A''%}' # one line up
+spaceship_rprompt_prefix() {
+  echo -n '%{'$'\e[1A''%}'
+}
+spaceship_rprompt_suffix() {
+  echo -n '%{'$'\e[1B''%}'
+}
 
 SPACESHIP_BATTERY_THRESHOLD=20
 SPACESHIP_EXIT_CODE_SHOW=true
