e03d38d851bcf2982dc01fe9a7cba34b436bb570
Elias Martinez Cohen <emc@eliath.biz>
2021-02-25 14:02:53 -0500

    Push exit code up to top line

--- zsh/spaceship.zsh
+++ zsh/spaceship.zsh
@@ -4,10 +4,10 @@ SPACESHIP_PROMPT_ORDER=(
   dir           # Current directory section
   host          # Hostname section
   git           # Git section (git_branch + git_status)
+  exit_code     # Exit code section
   line_sep      # Line break
   vi_mode       # Vi-mode indicator
   jobs          # Background jobs indicator
-  exit_code     # Exit code section
   char          # Prompt character
 )
 
