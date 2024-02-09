87e48080bf6482859009b8c5f3aa4bc39f9b3761
Elias Martinez Cohen <emc@eliath.biz>
2024-02-09 14:39:12 -0700

    Add function to list currently loaded ssh keys

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -51,3 +51,14 @@ notify-done() {
   last_cmd="$(echo $history[$HISTCMD] | sed 's/notify-done//')"
   osascript -e 'display notification "'"${last_cmd}"'" with title "Done"'
 }
+
+# Show info on ssh keys tracked by ssh-add -l
+# https://unix.stackexchange.com/a/566474
+ssh-listkeys() {
+    while read -r line; do
+        for file in ~/.ssh/*.pub;
+        do
+            printf "%s %s\n" "$(ssh-keygen -lf "$file" | awk '{$1=""}1')" "$file";
+        done | column -t | grep --color=auto "$line" || echo "$line";
+    done < <(ssh-add -l | awk '{print $2}')
+}
